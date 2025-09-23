8672e2fedafd9541b95f6ef743dc46d6a61e7069
arun.tiwari123 <arunstiwari@gmail.com>
2025-09-23 09:01:06 +0530

    week2-day1

--- src/main/java/com/fil/springbasic/service/OrderService.java
+++ src/main/java/com/fil/springbasic/service/OrderService.java
@@ -2,23 +2,43 @@ package com.fil.springbasic.service;
 
 import com.fil.springbasic.order.entity.Order;
 import com.fil.springbasic.order.entity.Product;
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 import java.util.List;
-
+@Configuration
 @Service
 public class OrderService {
+//    @Autowired
     private PricingService pricingService;
 
     public OrderService(PricingService pricingService) {
         this.pricingService = pricingService;
     }
 
+    @PostConstruct
+    public void postCreation(){
+        System.out.println("OrderService Object has been created");
+    }
+
     public Order createOrder(List<Product> products, String customerId) {
         BigDecimal totalAmount = pricingService.getTotalAmount(products);
         Order order = new Order(null, customerId, products, totalAmount);
         return order;
+    }
+
+    @Autowired
+    public void setPricingService(PricingService pricingService) {
+        this.pricingService = pricingService;
+    }
+
+    @PreDestroy
+    public void destroy(){
+        System.out.println("Order Service Destroyed");
     }
 }
--- src/test/java/com/fil/springbasic/order/service/OrderProcessingServiceTest.java
+++ src/test/java/com/fil/springbasic/order/service/OrderProcessingServiceTest.java
@@ -2,20 +2,26 @@ package com.fil.springbasic.order.service;
 
 import com.fil.springbasic.order.entity.Order;
 import com.fil.springbasic.order.entity.Product;
+import com.fil.springbasic.service.DatabaseService;
 import com.fil.springbasic.service.OrderService;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.math.BigDecimal;
 import java.util.List;
 
+@ActiveProfiles("qa")
 @SpringBootTest
 public class OrderProcessingServiceTest {
 
     @Autowired
     private OrderService orderService;
+
+    @Autowired
+    private DatabaseService databaseService;
     @Test
     void placeOrderTest(){
 
@@ -25,9 +31,25 @@ public class OrderProcessingServiceTest {
                 new Product("1", "TV", new BigDecimal(200.25), "Electronics"),
                 new Product("2", "Laptop", new BigDecimal(100.25), "Electronics")
         );
+        System.out.println("OrderService hashcode: "+orderService.hashCode());
+        Order createdOrder =  orderService.createOrder(products, "cust-1");
+
+        Assertions.assertEquals(new BigDecimal(200.25).add(new BigDecimal(100.25)) , createdOrder.getTotalAmount());
+        System.out.println(databaseService.getName());
+    }
+
+    @Test
+    void placeOrderTest1(){
+
+        long id = 1;
+        String customerId = "cust-1";
+        List<Product> products = List.of(
+                new Product("1", "TV", new BigDecimal(200.25), "Electronics"),
+                new Product("2", "Laptop", new BigDecimal(100.25), "Electronics")
+        );
+        System.out.println("OrderService hashcode: "+orderService.hashCode());
         Order createdOrder =  orderService.createOrder(products, "cust-1");
 
         Assertions.assertEquals(new BigDecimal(200.25).add(new BigDecimal(100.25)) , createdOrder.getTotalAmount());
     }
-
 }
