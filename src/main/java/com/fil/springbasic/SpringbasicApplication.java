b4cc195539f34b6eddea63c564c7dc33c34e293c
arun.tiwari123 <arunstiwari@gmail.com>
2025-09-23 09:03:28 +0530

    week2-day1

--- src/main/java/com/fil/springbasic/SpringbasicApplication.java
+++ src/main/java/com/fil/springbasic/SpringbasicApplication.java
@@ -1,18 +1,12 @@
 package com.fil.springbasic;
 
-import com.fil.springbasic.service.BankAccountService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 @SpringBootApplication
 public class SpringbasicApplication {
 
     public static void main(String[] args) {
-//        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
-//        BankAccountService bankAccountService = context.getBean(BankAccountService.class);
-//        System.out.println(bankAccountService.getBalance("12345"));
         SpringApplication.run(SpringbasicApplication.class, args);
     }
 
--- src/main/java/com/fil/springbasic/service/NotificationService.java
+++
@@ -1,5 +0,0 @@
-package com.fil.springbasic.service;
-
-public interface NotificationService {
-    boolean sendMessage(String emailId);
-}
